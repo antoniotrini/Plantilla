9defb936b424039f9bccb00f59b764a31a53fd83
Antonio Trinidad <trinity3.at@gmail.com>
2016-02-05 19:45:35 +0100

    ledPin
    Correció variable Pin digital 13
    El primer programa

--- El_primer_programa/El_primer_programa.ino
+++ El_primer_programa/El_primer_programa.ino
@@ -1,21 +1,26 @@
 /*********************************************************************************************
 **                                                                                          **
-** Parpellejar                                                                              ** 
+**                                 Blink / Parpellejar                                      ** 
 ** S'encén un LED durant un segon, després s'apaga durant un segon, en repetides ocasions.  **
 **                                                                                          **
-**********************************************************************************************/
+*********************************************************************************************/
+
+
+//**********  Variables  *********************************************************************
+int ledPin=13;
+
 
 // La funció de setup s'executa un cop al prémer reinici o el tauler
 void setup(){
   // Inicialitzar pin digital 13 com una sortida.
-    pinMode(13, OUTPUT);
+    pinMode(ledPin, OUTPUT);
 }
 
 
 // La funció de loop s'executa una vegada i una altra per sempre
 void loop(){
-  digitalWrite(13, HIGH);    // Encén el LED ( ALTA és el nivell de tensió )
+  digitalWrite(ledPin, HIGH);    // Encén el LED ( ALTA és el nivell de tensió )
   delay(1000);               // Esperar un segon
-  digitalWrite(13, LOW);     // Apagar el LED fent que la tensió BAIXA
+  digitalWrite(ledPin, LOW);     // Apagar el LED fent que la tensió BAIXA
   delay(1000);               // Esperar un segon
 }
